c675401fd5d8b77e00b5e7ea1ebf186cbe01d6a8
Peter Farmer <peter.m.farmer.204@gmail.com>
2024-01-24 23:14:27 +1100

    unicafe part 2 complete

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -4,16 +4,40 @@ function App() {
     const [good, setGood] = useState(0);
     const [neutral, setNeutral] = useState(0);
     const [bad, setBad] = useState(0);
+    const [all, setAll] = useState(0);
+    const [average, setAverage] = useState(0);
+    const [positive, setPositive] = useState(0);
 
     function onGoodClick() {
-        setGood(good + 1);
+        const nextGood = good + 1;
+        setGood(nextGood);
+
+        const nextAll = nextGood + neutral + bad;
+        setAll(nextAll);
+
+        setAverage((nextGood - bad) / nextAll);
+        setPositive((nextGood / nextAll) * 100);
     }
     function onNeutralClick() {
-        setNeutral(neutral + 1);
+        const nextNeutral = neutral + 1;
+        setNeutral(nextNeutral);
+
+        const nextAll = good + nextNeutral + bad;
+        setAll(nextAll);
+
+        setAverage((good - bad) / nextAll);
+        setPositive((good / nextAll) * 100);
     }
 
     function onBadClick() {
-        setBad(bad + 1);
+        const nextBad = bad + 1;
+        setBad(nextBad);
+
+        const nextAll = good + neutral + nextBad;
+        setAll(nextAll);
+
+        setAverage((good - nextBad) / nextAll);
+        setPositive((good / nextAll) * 100);
     }
 
     return (
@@ -32,6 +56,9 @@ function App() {
             <div>good {good}</div>
             <div>neutral {neutral}</div>
             <div>bad {bad}</div>
+            <div>all {all}</div>
+            <div>average {average}</div>
+            <div>positive {positive} %</div>
         </div>
     );
 }
