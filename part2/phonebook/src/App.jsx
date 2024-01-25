485ced18b6be2bbf76a48a048bffb72746ac02d3
Peter Farmer <peter.m.farmer.204@gmail.com>
2024-01-25 16:37:07 +1100

    phonebook step 1 complete

---
+++ part2/phonebook/src/App.jsx
@@ -0,0 +1,37 @@
+import { useState } from "react";
+
+function App() {
+    const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
+    const [newName, setNewName] = useState("");
+
+    function handleTextChange(e) {
+        setNewName(e.target.value);
+    }
+
+    function handleFormSubmit(e) {
+        e.preventDefault();
+        const newPersons = [...persons];
+        newPersons.push({ name: newName });
+        setPersons(newPersons);
+    }
+
+    return (
+        <div>
+            <h2>Phonebook</h2>
+            <form onSubmit={handleFormSubmit}>
+                <div>
+                    name: <input value={newName} onChange={handleTextChange} />
+                </div>
+                <div>
+                    <button type="submit">add</button>
+                </div>
+            </form>
+            <h2>Numbers</h2>
+            {persons.map((person) => {
+                return <p key={person.name}>{person.name}</p>;
+            })}
+        </div>
+    );
+}
+
+export default App;
