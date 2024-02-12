b01ffacd851ca272e5b9533cd78b2a5e0d431a0f
Peter Farmer <peter.m.farmer.204@gmail.com>
2024-02-12 12:12:07 +1100

    finished 4.4

--- part4/utils/list_helper.js
+++ part4/utils/list_helper.js
@@ -2,6 +2,15 @@ function dummy(blogs) {
   return 1;
 } 
 
+function totalLikes(blogs) {
+  if (blogs.length === 0) {
+    return 0;
+  }
+
+  return blogs.reduce((acc, curr) => acc += curr.likes, 0);
+}
+
 module.exports = {
-  dummy
+  dummy,
+  totalLikes
 }
