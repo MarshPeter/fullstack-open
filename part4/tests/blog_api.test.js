b95e05391209d517745ea1d414d0eb2d40ed6cf7
Peter Farmer <peter.m.farmer.204@gmail.com>
2024-02-15 17:20:32 +1100

    finished 4.9 and 4.10 (with jest)

--- part4/tests/blog_api.test.js
+++ part4/tests/blog_api.test.js
@@ -25,6 +25,34 @@ test('correct number of blogs are returned without modification to blog database
   expect(response.body).toHaveLength(3)
 })
 
+test('ensure that all blogs are returned with an "id" key', async () => {
+  const response = await api
+    .get('/api/blogs')
+    .expect(200)
+
+    response.body.forEach((blog) => expect(blog.id).toBeDefined())
+})
+
+test('ensure that new blogs are successfully added to the database', async () => {
+  const newBlog = {
+    title: "A new day for javascript",
+    author: "Joey Wheeler",
+    url: "https://test-blogs.com/agjlaskd98985fasd4g5678asd76f",
+    likes: 13
+  }
+
+  await api
+    .post('/api/blogs')
+    .send(newBlog)
+    .expect(201)
+
+  const response = await api
+    .get('/api/blogs')
+    .expect(200)
+
+  expect(response.body).toHaveLength(4)
+})
+
 afterAll(async () => {
   await mongoose.connection.close()
 })
