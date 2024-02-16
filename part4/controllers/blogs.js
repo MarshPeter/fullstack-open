216dd458f4a2ec3ca1de58e17ad58d99afbf1159
Peter Farmer <peter.m.farmer.204@gmail.com>
2024-02-16 21:50:19 +1100

    finished everything up to 4.13-4.14 (with jest)

--- part4/controllers/blogs.js
+++ part4/controllers/blogs.js
@@ -15,4 +15,28 @@ blogsRouter.post('/', async (request, response) => {
   response.status(201).json(result)
 })
 
+blogsRouter.delete('/:id', async (request, response) => {
+  const result = await Blog.findByIdAndDelete(request.params.id)
+
+  response.status(200).send('success')
+})
+
+blogsRouter.put('/:id', async (request, response) => {
+  const body = request.body
+  const id = request.params.id
+
+  const blog = {
+    title: body.title,
+    author: body.author,
+    url: body.url,
+    likes: body.likes
+  }
+
+  await Blog.findByIdAndUpdate(id, blog, { new: true })
+
+  response.status(201).send('success')
+})
+
+
+
 module.exports = blogsRouter
--- part4/tests/blog_api.test.js
+++ part4/tests/blog_api.test.js
@@ -15,7 +15,7 @@ beforeEach(async () => {
   await Promise.all(promiseArray)
 
   console.log('database populated')
-})
+}, 10000)
 
 test('correct number of blogs are returned without modification to blog database', async () => {
   const response = await api
@@ -52,6 +52,51 @@ test('ensure that new blogs are successfully added to the database', async () =>
 
   expect(response.body).toHaveLength(4)
 })
+
+test('ensure that blogs can be successfully deleted', async () => {
+  console.log('start');
+  const blogs = await Blog.find({})
+  console.log('found blogs');
+  let id = blogs[0].toJSON().id
+  console.log(`id is: ${id}`);
+
+  await api
+    .delete(`/api/blogs/${id}`)
+    .expect(200)
+
+  console.log('deleted response')
+  const response = await api
+    .get('/api/blogs')
+    .expect(200)
+
+  expect(response.body).toHaveLength(2)
+})
+
+test('update blogs by id', async () => {
+  const updatedBlog = {
+    title: "Functional javascript methods",
+    author: "Joey Wheeler",
+    url: "https://test-blogs.com/jgasd8y1235",
+    likes: 30
+  }
+
+  const response = await api
+    .get('/api/blogs')
+    .expect(200)
+
+  const id = response.body[0].id
+
+  await api
+    .put(`/api/blogs/${id}`)
+    .send(updatedBlog)
+    .expect(201)
+
+  const newResponse = await api
+    .get('/api/blogs')
+    .expect(200)
+
+  expect(newResponse.body[0].likes).toBe(30)
+}, 10000)
 
 afterAll(async () => {
   await mongoose.connection.close()
